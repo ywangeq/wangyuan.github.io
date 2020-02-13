fa0e0048b6c33b955fbf166354301eeaaabc23a1
ywangeq <ywangeq@connect.ust.hk>
2020-02-13 11:43:51 +0800

    update

---
+++ _posts/2020-02-12-1278分割回文串.py
@@ -0,0 +1,21 @@
+---
+layout:     post
+title:      Leecode 股票买卖问题
+subtitle:   1278 分割回文串III
+date:       2020-02-12
+author:     WY
+header-img: img/post-bg-ios9-web.jpg
+catalog: true
+tags:
+    - python
+    - 动态规划
+    - 字符串
+---
+给你一个由小写字母组成的字符串 s，和一个整数 k。
+
+请你按下面的要求分割字符串：
+
+首先，你可以将 s 中的部分字符修改为其他的小写英文字母。
+接着，你需要把 s 分割成 k 个非空且不相交的子串，并且每个子串都是回文串。
+请返回以这种方式分割字符串所需修改的最少字符数。
+
