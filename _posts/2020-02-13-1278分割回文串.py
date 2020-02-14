8229a859e20e26a93b05b9f8fd2069e7eb37f992
ywangeq <ywangeq@connect.ust.hk>
2020-02-14 11:08:29 +0800

    update

--- _posts/2020-02-13-1278分割回文串.py
+++ _posts/2020-02-13-1278分割回文串.py
@@ -1,8 +1,8 @@
 ---
 layout:     post
 title:      Leecode
-subtitle:   1278 分割回文串III
-date:       2020-02-12
+subtitle:   1278 分割回文串III 813最大平均值和的分组
+date:       2020-02-13
 author:     WY
 header-img: img/post-bg-ios9-web.jpg
 catalog: true
@@ -64,4 +64,36 @@ class Solution:
             if k> i+1: return float('inf')
             return min([dp[j,k-1]+cost(j+1,i) for j in range(i)])
         return dp(len(s)-1,k)
+```
+
+我们将给定的数组 A 分成 K 个相邻的非空子数组 ，我们的分数由每个子数组内的平均值的总和构成。计算我们所能得到的最大分数是多少。
+
+注意我们必须使用 A 数组中的每一个数进行分组，并且分数不一定需要是
+。
+示例:
+输入: 
+A = [9,1,2,3,9]
+K = 3
+输出: 20
+解释: 
+A 的最优分组是[9], [1, 2, 3], [9]. 得到的分数是 9 + (1 + 2 + 3) / 3 + 9 = 20.
+我们也可以把 A 分成[9, 1], [2], [3, 9].
+这样的分组得到的分数为 5 + 2 + 6 = 13, 但不是最大值.
+- 思路类似也是break 点去看
+
+
+```
+def largetave(A,K):
+    n = len(A)
+    S = [0]*(n+1)
+    dp =[[0]*(K+1) for i in range(n+1)]
+    for i in range(1,n+1):
+        S[i] = S[i-1]+A[i-1]
+        dp[i][1] = S[i]/i
+    for m in range(2,K+1):# 分多少个
+        for i in range(m,n+1):# 至少要有m个
+            for j in range(m-1,i):# 要在i上分 那长度至少m-1
+                dp[i][m] = max(dp[i][m],dp[j][m-1]+(S[i]-S[j])/(i-j))
+    return dp[n][K]
+
 ```
