ddc4ef932d958fd285fe60ea1f9cbe7f1180f376
ywangeq <ywangeq@connect.ust.hk>
2020-02-15 22:47:52 +0800

    update imge

---
+++ _posts/2020-02-15-1092-最短公共超序列.py
@@ -0,0 +1,25 @@
+---
+layout:     post
+title:      Leecode
+subtitle:   1092 最短公共超序列
+date:       2020-02-15
+author:     WY
+header-img: img/post-bg-ios9-web.jpg
+catalog: true
+tags:
+    - python
+    - 动态规划
+    - 记忆化递归
+---
+
+给出两个字符串 str1 和 str2，返回同时以 str1 和 str2 作为子序列的最短字符串。如果答案不止一个，则可以返回满足条件的任意一个答案。
+
+（如果从字符串 T 中删除一些字符（也可能不删除，并且选出的这些字符可以位于 T 中的 任意位置），可以得到字符串 S，那么 S 就是 T 的子序列）
+
+输入：str1 = "abac", str2 = "cab"
+输出："cabac"
+解释：
+str1 = "abac" 是 "cabac" 的一个子串，因为我们可以删去 "cabac" 的第一个 "c"得到 "abac"。 
+str2 = "cab" 是 "cabac" 的一个子串，因为我们可以删去 "cabac" 末尾的 "ac" 得到 "cab"。
+最终我们给出的答案是满足上述属性的最短字符串。
+
